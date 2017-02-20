c941999d94c6c1529cd2e4693d61c8fa448fad0c
mirrorZ <chandrika3437@gmail.com>
2017-02-20 23:32:50 +0900

    openssl public encryption private decryption

--- crypto.cpp
+++ crypto.cpp
@@ -8,83 +8,90 @@
 #include <openssl/err.h>
 
 class Crypto {
-  RSA * peer_pub;
-  RSA * private_key;
-
 public:
+  RSA *peer_pub;
+  RSA *private_key;
   int padding = RSA_PKCS1_PADDING;
   RSA * createRSA(char* filename, bool pub);   // Creates RSA structure from key file
-  int encrypt(unsigned char *data, int data_len, unsigned char *encrypted );
-  int decrypt(unsigned char *data, int data_len, unsigned char *decrypted);
+  int encrypt(unsigned char *data, unsigned int data_len, unsigned char *encrypted);
+  int decrypt(unsigned char *data, unsigned int data_len, unsigned char *decrypted);
 };
 
 RSA * Crypto::createRSA(char* filename, bool pub){
   FILE *fp = fopen(filename, "rb");
+  OpenSSL_add_all_algorithms();
+  OpenSSL_add_all_ciphers();
+  ERR_load_crypto_strings();
   if(fp==NULL) {
     perror(strerror(errno));
-    std::cout<<"Reading "<<filename<<std::endl;
     perror("key file does not exist\n");
     exit(1);
   }
-  
+
+  RSA *rsa = RSA_new();
   if(pub) {
-    RSA *peer_pub = RSA_new();
-    peer_pub = PEM_read_RSA_PUBKEY(fp, &peer_pub, NULL, NULL);
-    std::cout<<RSA_size(peer_pub)<<std::endl;
-    return peer_pub;
+    rsa = PEM_read_RSA_PUBKEY(fp, &rsa, NULL, NULL);
+    this->peer_pub = rsa;
+    return this->peer_pub;
   }
   else {
-    RSA *private_key = RSA_new();
-    private_key = PEM_read_RSAPrivateKey(fp, &private_key, NULL, NULL);
-    return private_key;
+    rsa = PEM_read_RSAPrivateKey(fp, &rsa, NULL, NULL);
+    if (rsa == NULL) {
+      std::cout<<"privateKey could not be read"<<std::endl;
+      exit(1);
+    }
+    this->private_key = rsa;
+    return this->private_key;
   }
   return NULL;
 }
 
-int Crypto::encrypt(unsigned char *data, int data_len, unsigned char *encrypted){
+int Crypto::encrypt(unsigned char *data, unsigned int data_len, unsigned char *encrypted){
   int rval;
-  data_len = sizeof(* data);
-  std::cout<<"got here"<<std::endl;
-  rval = RSA_public_encrypt(data_len, data, encrypted, peer_pub, padding);
+  rval = RSA_public_encrypt(data_len, data, encrypted, this->peer_pub, padding);
   if(rval==-1){
-    std::cout<<"Encryption failure"<<ERR_get_error()<<std::endl;
-    std::cout<<"Encryption failure"<<std::endl;
+    std::cout<<"Encryption failure: "<<ERR_get_error()<<std::endl;
     exit(1);
   }
- 
-  // std::cout<<"Encrytped message"<<encrypted<<std::endl;
 }
 
-int Crypto::decrypt(unsigned char *data, int data_len, unsigned char *decrypted){
-   int rval;
-   decrypted = (unsigned char*)malloc(RSA_size(private_key));
-   if(decrypted == NULL) {
-     std::cout<<"Failed to allocate memory"<<std::endl;
-     exit(1);
-   }
-
-   rval = RSA_public_encrypt(data_len,  (const unsigned char *)data, decrypted, private_key, padding);
+int Crypto::decrypt(unsigned char *data, unsigned int data_len, unsigned char *decrypted){
+  int rval;
+  rval = RSA_private_decrypt(data_len,  (const unsigned char *)data, decrypted, private_key, padding);
    if(rval==-1){
-     // std::cout<<"Encryption failure"<<err_get_error()<<std::endl;
-     std::cout<<"Encryption failure"<<std::endl;
+     std::cout<<"Decryption failure"<<ERR_get_error()<<std::endl;
      exit(1);
    }
-    std::cout<<"Decrytped message"<<decrypted<<std::endl;
 }
 
-int main() {
+int main(int argc, char *argv[]) {
+  if(argc<3) {
+    std::cout<<"Usage: /path/to/private/pem /path/to/peer/public/pem message"<<std::endl;
+    exit(1);
+  }
   Crypto c;
-  char *msg= "hello";
-  RSA *pr = c.createRSA("/home/mirrorz/.ssh/private.pem", false);
-  RSA *pb = c.createRSA("/home/mirrorz/.ssh/public.pem", true);
-
-  std::cout<<RSA_size(pb)<<std::endl;
-  unsigned char *encrypted = (unsigned char*)malloc(256);
+  
+  RSA *pr = c.createRSA(argv[1], false);
+  RSA *pb = c.createRSA(argv[2], true);
+  
+  unsigned char *msg = (unsigned char*)strdup("secret message");
+  int pb_size = RSA_size(pb);
+  unsigned char *encrypted = (unsigned char*)malloc(pb_size);
   if(encrypted == NULL) {
     std::cout<<"Failed to allocate memory"<<std::endl;
     exit(1);
-    }
-  std::cout<<sizeof(encrypted)<<std::endl;
-  c.encrypt((unsigned char *)&msg, 5, encrypted);//(unsigned char *)&dmsg);
+   }
+  c.encrypt(msg, strlen((const char *)msg), encrypted);
+  std::cout<<"Encrypted message:"<<encrypted<<std::endl;
+  
+  int pr_size = RSA_size(pr);
+  unsigned char *decrypted = (unsigned char*)malloc(pr_size);
+  if(decrypted == NULL) {
+    std::cout<<"Failed to allocate memory"<<std::endl;
+    exit(1);
+   }
+  c.decrypt(encrypted, pr_size, decrypted);
+  std::cout<<"Decrypted message: "<<decrypted<<std::endl;
+  
   return 0;
 }
