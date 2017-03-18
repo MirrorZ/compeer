089934954db496f7ed898031b4bca92892704288
Ashish Gupta <ashmew2@gmail.com>
2017-03-18 22:56:32 +0100

    Fix up compilation aliases to work across branches with toggle-able flag

--- compeer.bash
+++ compeer.bash
@@ -5,6 +5,13 @@
 # Add to ~/.bashrc:
 # echo 'source $HOME/compeer/compeer.bash' >> $HOME/.bashrc
 
-alias mkcm='rm -f compeer; g++ crypto.cpp peer.cpp -o compeer -Wall'
-alias cml='./compeer -listen localhost 20000 -encrypt'
-alias cmf='./compeer -friend localhost 20000 -encrypt'
+unset ENCFLAG
+# Set to 0 to disable.
+ENCRYPT=0
+
+CPP_FILES=$(git ls-files *.cpp | grep -Fv 'peer.cpp')
+[[ "$ENCRYPT" -eq 1 ]] && ENCFLAG=-encrypt
+
+alias mkcm="rm -f compeer; g++ peer.cpp $CPP_FILES -o compeer -Wall"
+alias cml='./compeer -listen localhost 20000 $ENCFLAG'
+alias cmf='./compeer -friend localhost 20000 $ENCFLAG'
