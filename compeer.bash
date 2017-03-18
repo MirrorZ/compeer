98b10d1420c3ac12c61520d09ed1e59d7dae93af
Ashish Gupta <ashmew2@gmail.com>
2017-03-19 00:02:10 +0100

    Fix encrypted build aliases:

--- compeer.bash
+++ compeer.bash
@@ -12,6 +12,6 @@ ENCRYPT=0
 CPP_FILES=$(git ls-files *.cpp | grep -Fv 'peer.cpp')
 [[ "$ENCRYPT" -eq 1 ]] && ENCFLAG=-encrypt
 
-alias mkcm="rm -f compeer; g++ peer.cpp $CPP_FILES -o compeer -Wall"
+alias mkcm="rm -f compeer; g++ peer.cpp $CPP_FILES -o compeer -Wall -lcrypto"
 alias cml='./compeer -listen localhost 20000 $ENCFLAG'
 alias cmf='./compeer -friend localhost 20000 $ENCFLAG'
