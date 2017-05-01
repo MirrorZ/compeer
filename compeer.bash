d9cd487acf6e9e841d6a8224d8543b9304142388
Ashish Gupta <ashmew2@gmail.com>
2017-05-01 19:53:32 +0200

    Fix aliases

--- compeer.bash
+++ compeer.bash
@@ -12,7 +12,6 @@ ENCRYPT=1
 CPP_FILES=$(git ls-files *.cpp | grep -Fv 'peer.cpp')
 [[ "$ENCRYPT" -eq 1 ]] && ENCFLAG=-encrypt
 
-alias mkcm="rm -f compeer; g++ peer.cpp $CPP_FILES -o compeer -Wall -lcrypto"
 alias cml='./compeer -listen localhost 20000 $ENCFLAG'
 alias cmf='./compeer -friend localhost 20000 $ENCFLAG'
-
+alias mkcm='rm -f compeer; g++ peer.cpp crypto.cpp util.cpp -o compeer -Wall -lcrypto'
